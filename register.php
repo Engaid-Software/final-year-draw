74e53dfd9d1bc62884100e20e8e902f345faf492
SweetTreats <sumite3117@hotmail.com>
2018-12-26 18:37:37 +0000

    Design improvements

--- register.php
+++ register.php
@@ -110,32 +110,6 @@ style="background-size: cover ;background-repeat: no-repeat;min-height:auto;">
 
 <input type='hidden' name='submitted' id='submitted' value='1'/>
 
-<!--
-<div class='container'>
-    <label for='name' >Your Full Name*: </label><br/>
-    <input type='text' name='name' id='name' value='<?php echo $fgmembersite->SafeDisplay('name') ?>' maxlength="50" /><br/>
-    <span id='register_name_errorloc' class='error'></span>
-</div>
-<div class='container'>
-    <label for='email' >Email Address*:</label><br/>
-    <input type='text' name='email' id='email' value='<?php echo $fgmembersite->SafeDisplay('email') ?>' maxlength="50" /><br/>
-    <span id='register_email_errorloc' class='error'></span>
-</div>
-<div class='container'>
-    <label for='username' >UserName*:</label><br/>
-    <input type='text' name='username' id='username' value='<?php echo $fgmembersite->SafeDisplay('username') ?>' maxlength="50" /><br/>
-    <span id='register_username_errorloc' class='error'></span>
-</div>
-<div class='container' style='height:80px;'>
-    <label for='password' >Password*:</label><br/>
-    <div class='pwdwidgetdiv' id='thepwddiv' ></div>
-    <noscript>
-    <input type='password' name='password' id='password' maxlength="50" />
-    </noscript>    .
-    <div id='register_password_errorloc' class='error' style='clear:both'></div>
-</div>
-   --->
-
 	<div class="row">
 		<div class="col-sm-9" >
 			<h1 align="left" style="color:#7fa2db;font-family: 'Comic Sans MS', 'Comic Sans', cursive;" >
